53e9ba12f2b98b5151a6479482141343d5a4ff2b
loochief <lopezevencio@gmail.com>
2024-03-13 21:51:17 -0500

    added nat gateway, private subnet and private route table

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Create a VPC
+#Section Create a VPC
 resource "aws_vpc" "First_VPC" {
 
   cidr_block = "192.168.0.0/16"
@@ -20,7 +20,7 @@ resource "aws_vpc" "First_VPC" {
 
 
   tags = {
-    Name = "First VPC"
+    Name = "Dev VPC"
     Terraform = "true"
     Environment = "Dev"
   }
@@ -29,6 +29,7 @@ resource "aws_vpc" "First_VPC" {
   enable_dns_support = true
 }
 
+#Section creates a public subnet
 resource "aws_subnet" "public" {
   vpc_id = aws_vpc.First_VPC.id
   cidr_block = "192.168.1.0/24"
@@ -41,7 +42,8 @@ resource "aws_subnet" "public" {
   }
 }
 
-resource "aws_internet_gateway" "gw" {
+#Section Creates an Internet Gateay in the public subnet
+resource "aws_internet_gateway" "internet_gw" {
   vpc_id = aws_vpc.First_VPC.id
 
   tags = {
@@ -50,17 +52,61 @@ resource "aws_internet_gateway" "gw" {
     Environment = "Dev"
   }
 }
+#Section Creates a NAT Gateway in the public subnet
+resource "aws_eip" "nat_gateway_eip" {
+  domain = "vpc"
+}
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_gateway_eip.id
+  subnet_id     = aws_subnet.public.id
 
+  tags = {
+    Name = "NAT gw"
+  }
+  depends_on = [aws_internet_gateway.internet_gw, aws_eip.nat_gateway_eip]
+  
+}
+
+#Section creates a private subnet
+resource "aws_subnet" "private" {
+  vpc_id = aws_vpc.First_VPC.id
+  cidr_block = "192.168.4.0/24"
+  availability_zone_id = "use1-az1"
+
+  tags = {
+    Name = "private_subnet"
+    Terraform = "true"
+    Environment = "Dev"
+  }
+}
+
+#Section creates a public route table with a default route to Internet Gateway
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.First_VPC.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
+    gateway_id = aws_internet_gateway.internet_gw.id
   }
   tags = {
     Name = "public_rt"
     Terraform = "true"
     Environment = "Dev"
   }
+}
+
+#Section creates a private route table with a default route to the NAT gateway
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.First_VPC.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_gw.id
+  }
+  tags = {
+    Name = "private_rt"
+    Terraform = "true"
+    Environment = "Dev"
+  }
+  depends_on = [aws_nat_gateway.nat_gw]
 }
